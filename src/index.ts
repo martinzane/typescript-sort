7a1a505b3435392bd7e869edfdad21c32d5f3ffe
Martin Zane <martin@zaneweb.com>
2024-04-25 16:22:34 +0200

    Implement a basic Sorter with bubble sort for a collection of numbers

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello, world!");
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        const a = this.collection[j];
+        const b = this.collection[j + 1];
+
+        if (a > b) {
+          this.collection[j] = b;
+          this.collection[j + 1] = a;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+
+console.log(sorter.collection);
